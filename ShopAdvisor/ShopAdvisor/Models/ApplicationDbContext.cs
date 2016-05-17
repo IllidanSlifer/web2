c77cbd6d27497f019ff7dcd721058981fb75ecab
Kevin Illidan <illidankj@hotmail.com>
2016-05-16 21:23:51 -0600

    Crud
    prueba de crud

---
+++ ShopAdvisor/ShopAdvisor/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopAdvisor.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+            public ApplicationDbContext()
+                : base("DefaultConection")
+            {
+                
+            }
+        public DbSet<Place> Places { set; get; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        }
+}
