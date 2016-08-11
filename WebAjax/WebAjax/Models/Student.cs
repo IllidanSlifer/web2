d3d5fb657ca4cbe4a1633570d0ac4ffd1aa4561f
Kevin Illidan <illidankj@hotmail.com>
2016-08-10 23:55:18 -0600

    quiz2

---
+++ WebAjax/WebAjax/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace WebAjax.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Phone { get; set; }
+        public string Adress { get; set; }
+        public string Email { get; set; }
+
+    }
+    class StudentsDbContext : DbContext {
+        public DbSet<Student> Students { get; set; }
+    }
+}
